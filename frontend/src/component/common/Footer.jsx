643196ebe5e4ada8e5659633aec1983a209cfa9d
adityaz01 <156804147+adityaz01@users.noreply.github.com>
2024-08-03 16:14:44 +0530

    Add files via upload

---
+++ frontend/src/component/common/Footer.jsx
@@ -0,0 +1,13 @@
+const FooterComponent = () => {
+
+
+    return (
+        <footer>
+            <span className="my-footer">
+                Royal Vista Hotel | All Right Reserved &copy; {new Date().getFullYear()}
+            </span>
+        </footer>
+    );
+};
+
+export default FooterComponent;
